35f732cae4c24250f6095fe99f290a6b28b256a4
Noah Gift <noah.gift@gmail.com>
2017-07-08 17:09:27 -0700

    adding R hello world example

---
+++ R/hello-world/README.md
@@ -0,0 +1,15 @@
+# Hello-World Command-line in R
+
+## Overview
+
+Depends on https://github.com/trevorld/optparse for R
+
+## Usage
+
+➜  hello-world git:(master) ✗ ./hello-world.R --count 5 --phrase "hello world"
+hello world
+hello world
+hello world
+hello world
+hello world
+
---
+++ R/hello-world/hello-world.R
@@ -0,0 +1,25 @@
+#!/usr/bin/env Rscript
+#Hello World R command-line tool
+#
+
+
+suppressPackageStartupMessages(library("optparse"))
+parser <- OptionParser()
+parser <- add_option(parser, c("-c", "--count"), type = "integer",
+                     help = "Number of times to print phrase",
+                     metavar = "number")
+parser <- add_option(parser, c("-p", "--phrase"),
+                    help = "Phrase to print")
+
+args <- parse_args(parser)
+
+
+# Function to Generate Phrases
+phrasegen <- function(arguments){
+    for (count in 1:arguments$count){
+        cat(paste(arguments$phrase, "\n"))
+    }
+}
+
+#Run the program
+phrasegen(args)
